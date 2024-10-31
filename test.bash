1e44e68692f648ee40b9c6b905aed31be6ed5464
naoto <naoto1221g@gmail.com>
2024-10-31 12:46:43 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,10 +1,20 @@
-#!/bin/bash
+#!/bin/bash 
  ng () {
 	 echo line ${1} was wrong
 	 res=1
 }
 res=0
+### 普通の入力 ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "LINENO"
-[ "${res}" = 0 ] && echo OK
+[ "${out}" = 15 ] || ng "$LINENO"
+### おかしな入力 ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus) 
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo All collect
 exit "$res"
