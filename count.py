b1e2dea92285992a648b177383db02cc9057ec41
naoto <naoto1221g@gmail.com>
2024-11-28 13:32:48 +0900

    Add command

---
+++ count.py
@@ -0,0 +1,23 @@
+#!/user/bin/python3
+# SPDX-FileCopyrightText: 2024 Oka Naoto
+from collections import Counter
+import string
+
+def count_alphabets(text):
+
+    text = text.lower()
+    filtered_text = ''.join(char for char in text if char in string.ascii_lowercase)
+    
+
+    counts = Counter(filtered_text)
+    
+    sorted_counts = {letter: counts.get(letter, 0) for letter in string.ascii_lowercase}
+    
+    return sorted_counts
+
+text = input("文章を入力してください: ")
+result = count_alphabets(text)
+
+for letter, count in result.items():
+    print(f"{letter}: {count}")
+
